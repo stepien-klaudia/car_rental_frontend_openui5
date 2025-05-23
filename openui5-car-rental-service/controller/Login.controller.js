200ba5db348c72a8d9ad9f235650adb548d5c823
Klaudia <stepien.klaudia.agata@gmail.com>
2025-05-23 22:00:48 +0200

    changes 23/05/2025 - main and login page

---
+++ openui5-car-rental-service/controller/Login.controller.js
@@ -0,0 +1,48 @@
+sap.ui.define(["sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/ui/core/BusyIndicator"], function (Controller, MessageToast) {
+    "use strict";
+    return Controller.extend("openui5-car-rental-service.controller.Login", {
+      onInit: function () {
+        this.getView().setModel(new sap.ui.model.json.JSONModel({
+          username: "",
+          password: ""
+        }));
+      },
+  
+      onLogin: function () {
+        sap.ui.core.BusyIndicator.show(0);
+        const oData = this.getView().getModel().getData();
+  
+        fetch("http://localhost:5000/api/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(oData)
+        })
+        .then(async res => {
+          if (!res.ok) {
+            throw new Error("Błędny login lub hasło");
+          }
+          return res.json();
+        })
+        .then(data => {
+          // Zapisz token w lokalnym storage (lub cookie)
+          localStorage.setItem("token", data.access_token);
+          MessageToast.show("Zalogowano pomyślnie!");
+          // Przejdź do widoku głównego
+          const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+          oRouter.navTo("home");
+        })
+        .catch(err => {
+          this.byId("statusText").setText(err.message);
+          this.byId("statusText").setType("Error");
+          this.byId("statusText").setVisible(true);
+        })
+        .finally( ()=>{
+            sap.ui.core.BusyIndicator.hide();
+        }
+        )
+        
+      }
+    });
+  });
--- openui5-car-rental-service/controller/Main.controller.js
+++ openui5-car-rental-service/controller/Main.controller.js
@@ -1,13 +1,7 @@
-// sap.ui.define([
-//     "sap/ui/core/mvc/Controller"
-//   ], function (Controller) {
-//     "use strict";
-//     return Controller.extend("openui5-car-rental-service.controller.Main", {});
-//   });
-
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
+    "sap/ui/core/BusyIndicator"
 ], function (Controller, MessageToast) {
     "use strict";
 
@@ -38,6 +32,10 @@ sap.ui.define([
             var sSelectKey = oEvent.getParameter("key");
             this.byId("flotaContent").setVisible(false);
             this.byId("clientContent").setVisible(false);
+            this.byId("pracownikContent").setVisible(false);
+            this.byId("rezerwacjaContent").setVisible(false);
+            this.byId("punktContent").setVisible(false);
+            this.byId("raportContent").setVisible(false);
 
             switch (sSelectKey){
                 case "zarzadzanie-flota":
@@ -46,11 +44,58 @@ sap.ui.define([
                 case "zarzadzanie-klientami":
                     this.byId("clientContent").setVisible(true);
                     break;
+                case "zarzadzanie-pracownikami":
+                    this.byId("pracownikContent").setVisible(true);
+                    break;
+                case "zarzadzanie-rezerwacjami":
+                    this.byId("rezerwacjaContent").setVisible(true);
+                    break;
+                case "zarzadzanie-punktami":
+                    this.byId("punktContent").setVisible(true);
+                    break;
+                case "raportowanie":
+                    this.byId("raportContent").setVisible(true);
+                    break;
             }
-            // if (sSelectKey == "zarzadzanie-flota"){
-            //     this.byId("flotaContent").setVisible(true);
-            // }
+        },
+        onProfilePress: function (oEvent){
+            const oButton = oEvent.getSource();
 
+            if (!this._oProfileMenu) {
+            this._oProfileMenu = new sap.m.Menu({
+            items: [
+            new sap.m.MenuItem({
+            text: "Zmień motyw na ciemny/jasny",
+            icon: "sap-icon://action-settings",
+            press: function () {
+            sap.ui.core.BusyIndicator.show(0);
+            var _oTheme = sap.ui.getCore().getConfiguration().getTheme();
+            if (_oTheme == "sap_fiori_3_dark"){
+                sap.ui.getCore().applyTheme("sap_fiori_3");
+            }
+            else{
+                sap.ui.getCore().applyTheme("sap_fiori_3_dark");
+            };
+            sap.ui.core.BusyIndicator.hide();
+            sap.m.MessageToast.show("Zmieniono motyw");
+            }
+            }),
+            new sap.m.MenuItem({
+            text: "Wyloguj",
+            icon: "sap-icon://log",
+            press: function () {
+            sap.m.MessageToast.show("Wylogowano");
+          }
+        })
+        ]
+        });
         }
+
+        this._oProfileMenu.openBy(oButton);
+        }
+    
+    
+    
     });
+        
 });
