869c3138c497d7de974fc31665feefe4e14f2d37
Klaudia <stepien.klaudia.agata@gmail.com>
2025-05-29 19:58:44 +0200

    changes 25/005/2025 editcar app

---
+++ openui5-car-rental-service/controller/EditCar.controller.js
@@ -0,0 +1,66 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/library",
+	"sap/m/Dialog",
+	"sap/m/Button",
+	"sap/m/library",
+	"sap/m/Text"
+  ], function (Controller, JSONModel, coreLibrary, Dialog, Button, mobileLibrary, Text) {
+    "use strict";
+    // shortcut for sap.m.ButtonType
+	var ButtonType = mobileLibrary.ButtonType;
+
+	// shortcut for sap.m.DialogType
+	var DialogType = mobileLibrary.DialogType;
+
+	// shortcut for sap.ui.core.ValueState
+	var ValueState = coreLibrary.ValueState;
+  
+    return Controller.extend("openui5-car-rental-service.controller.NewCar", {
+  
+      onInit: function () {
+        
+      },
+
+      onNavPress: function () {
+        this.getOwnerComponent().getRouter().navTo("Main",{},true);
+      },
+
+      onProfilePress: function (oEvent){
+        const oButton = oEvent.getSource();
+        var that = this;
+
+        if (!this._oProfileMenu) {
+        this._oProfileMenu = new sap.m.Menu({
+        items: [
+        new sap.m.MenuItem({
+        text: "Zmień motyw na ciemny/jasny",
+        icon: "sap-icon://action-settings",
+        press: function () {
+        sap.ui.core.BusyIndicator.show(0);
+        var _oTheme = sap.ui.getCore().getConfiguration().getTheme();
+        if (_oTheme == "sap_fiori_3_dark"){
+            sap.ui.getCore().applyTheme("sap_fiori_3");
+        }
+        else{
+            sap.ui.getCore().applyTheme("sap_fiori_3_dark");
+        };
+        sap.ui.core.BusyIndicator.hide();
+        sap.m.MessageToast.show("Zmieniono motyw");
+        }
+        }),
+        new sap.m.MenuItem({
+        text: "Wyloguj",
+        icon: "sap-icon://log",
+        press: () => {
+            that.getOwnerComponent().getRouter().navTo("Logoff", {}, true);
+      }
+    })
+    ]
+    });
+    }
+    this._oProfileMenu.openBy(oButton);
+    }
+    })
+  });
